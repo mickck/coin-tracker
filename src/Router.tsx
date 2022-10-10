7c9ad6caab9668b392e5336c51751c2de4f9f68c
Mickck <mikemwkim@gmail.com>
2022-10-10 19:53:01 +1100

    -

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,8 +5,8 @@ function Router() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/:coinId/*' element={<Coin />}></Route>
-        <Route path='/' element={<Coins />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/:coinId/*/`} element={<Coin />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />}></Route>
       </Routes>
     </BrowserRouter>
   );
